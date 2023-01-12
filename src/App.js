727e80cb1bdb758510d91d81b407379354576ef1
Harry Ward-Gray <wardgray24@gmail.com>
2023-01-12 22:00:03 +0000

    0.0.8 test preload

--- src/App.js
+++ src/App.js
@@ -12,11 +12,15 @@ import ReactLoading from "react-loading";
 export default class App extends React.Component {
   state = {
     loadingState: 1,
+    loadingHeight: '100vh',
+    loadingOverflow: 'hidden'
   }
   componentDidMount = () => {
     setTimeout(() => {
       this.setState({
         loadingState: 0,
+        loadingHeight: 'auto',
+        loadingOverflow: 'visible'
       })
     }, 1000)
 
@@ -24,7 +28,7 @@ export default class App extends React.Component {
   render() {
 
     return (
-      <div id="main" >
+      <div id="main" style={{ height: this.state.loadingHeight, overflow: this.state.loadingOverflow }}>
         <div id="loading" style={{ opacity: this.state.loadingState }}>
           <ReactLoading type="bubbles" color="white"
             height={100} width={50} />
