86eb2dd36f3dc240763536690e854bd25858a230
Harry Ward-Gray <wardgray24@gmail.com>
2023-01-12 19:16:03 +0000

    0.0.1 Loading screen implemented
    -Changed App to class based react component
    -Used componentDidMount method set loading state, and display loading screen

--- src/App.js
+++ src/App.js
@@ -5,19 +5,43 @@ import About from './components/pages/About'
 import Location from './components/pages/Location'
 import Contact from './components/pages/Contact'
 import Footer from './components/pages/Footer'
+import React, { useState } from 'react';
+import ReactLoading from "react-loading";
 
-function App() {
-  return (
-    <div id="main">
-      <Head />
-      <Home />
-      <Services />
-      <About />
-      <Location />
-      <Contact />
-      <Footer />
-    </div >
-  );
+
+export default class App extends React.Component {
+  state = {
+    loadingState: 1,
+    displayContent: 'none'
+  }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        loadingState: 0,
+        displayContent: 'block'
+      })
+
+    }, 500);
+
+  }
+  render() {
+
+    return (
+      <div id="main" >
+        <div id="loading" style={{ opacity: this.state.loadingState }}>
+          <ReactLoading type="bubbles" color="white"
+            height={100} width={50} />
+        </div>
+        <div style={{ display: this.state.displayContent }}>
+          <Head />
+          <Home />
+          <Services />
+          <About />
+          <Location />
+          <Contact />
+          <Footer />
+        </div>
+      </div >)
+  };
 }
 
-export default App;
